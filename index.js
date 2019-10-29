dc9149498d7f17396ac5ec202012ecbcf271518b
James McManamey <jfmcmanamey@gmail.com>
2019-10-28 23:50:31 -0700

    moved heading to svg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
-var margin = { top: 20, right: 30, bottom: 30, left: 100 },
+var margin = { top: 60, right: 30, bottom: 30, left: 30 },
   width = 1200,
-  height = 625;
+  height = 825;
 
 const title = "United States Educational Attainment";
 const desc =
@@ -15,10 +15,12 @@ const svg = d3
   .append("svg")
   .attr("title", title)
   .attr("description", desc)
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width - margin.left - margin.right)
+  .attr("height", height - margin.top - margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform",
+  "translate(60,"+margin.top +")")
+ 
 
 //draw counties
 
@@ -43,12 +45,37 @@ const drawChart = (us, edu) => {
     )
     .range(d3.schemeBlues[9]);
 
+    //
+const heading = svg.append('g')
+
+
+
+heading
+.append('text')
+.text(title)
+.attr("font-family", "sans-serif")
+.attr("font-size", "40px")
+.attr("fill", "grey")
+.style("text-anchor", "center")
+.attr("transform",
+"translate(220,0)")
+
+heading
+.append('text')
+.text(desc)
+.attr("font-family", "sans-serif")
+.attr("font-size", "16px")
+.attr("fill", "grey")
+.style("text-anchor", "center")
+.attr("transform",
+"translate(235,35)")
+
   //attach the legend
   const g = svg
     .append("g")
     .attr("class", "key")
     .attr("id", "legend")
-    .attr("transform", "translate(0,40)");
+    .attr("transform", "translate(0,60)");
 
   g.selectAll("rect")
     .data(
@@ -118,8 +145,8 @@ const drawChart = (us, edu) => {
     tooltip
       .transition("0")
       .style("opacity", "0.9")
-      .style("left", event.clientX + "px")
-      .style("top", event.clientY +20 + "px")
+      .style("left", event.clientX + 20+"px")
+      .style("top", event.clientY -30 + "px")
       .attr('data-education', eduData.bachelorsOrHigher)
     tooltip.html(`${eduData.area_name}, ${eduData.state} <br/> ${eduData.bachelorsOrHigher}`);
   };
@@ -140,6 +167,8 @@ const drawChart = (us, edu) => {
   svg
     .append("g")
     .attr("id", "Counties")
+    .attr("transform",
+"translate(0,30)")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.counties).features)
     .join("path")
@@ -158,6 +187,8 @@ const drawChart = (us, edu) => {
   //overlay the state borders
   svg
     .append("path")
+    .attr("transform",
+    "translate(0,30)")
     .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
     .attr("fill", "none")
     .attr("id", "States")
